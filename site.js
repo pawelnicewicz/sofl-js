608b1afc002b6bac3299777ad47530dbd026a8d3
Michal <michal.banaszek@op.pl>
2018-12-06 12:55:29 +0000

    Stworzenie plikow na podstrony

--- site.js
+++ site.js
@@ -2,3 +2,7 @@ exports.index = function(req, res){
   res.render("index", { description: "description" });
 }; 
 
+
+/*exports.about = function(req, res){
+  res.render("about", { description: "description" });
+};*/ 
