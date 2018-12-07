664160a21f1a561a67398ed0e91f23d0eb47f9a8
Paweł Nicewicz <nicewiczpawel@gmail.com>
2018-12-07 13:39:03 +0000

    some styling and folder structure changes

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express  = require("express"),
     app      = express(),
     mongoose = require("mongoose"),
     dotenv   = require("dotenv").config(),
+    path     = require("path"),
     site     = require("./site");
 
 mongoose.set('useNewUrlParser', true);
 mongoose.connect(process.env.DB_URL);
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
 // General
 app.get("/", site.index);
