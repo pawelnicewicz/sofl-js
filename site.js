6457fe8257b07f855e5ff820d12af0c2821a599e
Michal <banan541@gmail.com>
2018-12-17 13:20:47 +0000

    Add email validation

--- site.js
+++ site.js
@@ -1,3 +1,7 @@
+const nodeMailer = require('nodemailer'),
+      bodyParser = require('body-parser'),
+      validator = require("email-validator");
+
 exports.index = function(req, res){
   res.render("homepage/index");
 }; 
@@ -20,4 +24,37 @@ exports.careers = function(req, res){
 
 exports.contact = function(req, res){
   res.render("homepage/contact");
+};
+
+exports.sendMail = function(req, res){
+  let transporter = nodeMailer.createTransport({
+          host: 'smtp.wp.pl',
+          port: 465,
+          secure: true,
+          auth: {
+              user: process.env.MAIL_USERNAME,
+              pass: process.env.MAIL_PASS
+          }
+      });
+      
+       
+  let mailOptions = {
+          
+          from: process.env.MAIL_USERNAME, // sender address
+          to: process.env.MAIL_USERNAME, // list of receivers
+          subject: "Pytanie o kurs.", // Subject line
+          html: "Email: " + req.body.email + "<br>Message: " + req.body.message
+      };
+      
+      transporter.sendMail(mailOptions, (error, info) => {
+          if(validator.validate(req.body.email) == false){
+            return console.log('Email not valid');
+          }
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message %s sent: %s', info.messageId, info.response);
+              res.redirect("/");
+          });
+      
 };
