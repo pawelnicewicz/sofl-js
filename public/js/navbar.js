7863faa275a37a4332405fa21a3018c0cae90440
Paweł Nicewicz <nicewiczpawel@gmail.com>
2018-12-18 15:00:32 +0000

    added logout to navbar + front-end adjustments

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,25 +1,35 @@
+/* global showLogout */
+
 // When the user scrolls the page, execute navbarScroll 
 window.onscroll = function() {navbarScroll()};
 
 // When the user clicks the navbar expand button, execute navbarClick
 var button = document.getElementsByClassName("navbar-toggler")[0];
 button.onclick = function() {navbarClick()};
+var loginIcon = document.getElementById("login-icon");
+var logoutIcon = document.getElementById("logout-icon");
 
 // Get the header
 var navbar = document.getElementById("navbar");
+
+if(showLogout){
+  logoutIcon.classList.add("logout-icon-mobile");
+}
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function navbarScroll() {
   // Get the offset position of the navbar
   var sticky = navbar.offsetTop;
   var navHeight = navbar.offsetHeight;
-  var loginIcon = document.getElementById("login-icon");
   if (window.pageYOffset > sticky && window.pageYOffset < (navHeight + sticky)) {
     navbar.classList.add("sticky");
     if(button.getAttribute("aria-expanded") == "false"){
       navbar.classList.remove("navbar-transparent");
       navbar.classList.add("navbar-opaque");
       loginIcon.classList.add("hidden");
+      if(!showLogout) {
+        logoutIcon.classList.add("hidden");
+      }
     }
   }
   else if(window.pageYOffset > (navHeight + sticky)){
@@ -28,6 +38,9 @@ function navbarScroll() {
       navbar.classList.remove("navbar-opaque");
       navbar.classList.add("navbar-transparent");
       loginIcon.classList.remove("hidden");
+      if(showLogout) {
+        logoutIcon.classList.remove("hidden");
+      }
     }
   }
   else {
@@ -36,6 +49,9 @@ function navbarScroll() {
       navbar.classList.remove("navbar-transparent");
       navbar.classList.add("navbar-opaque");
       loginIcon.classList.add("hidden");
+      if(showLogout) {
+        logoutIcon.classList.add("hidden");
+      }
     }
   }
 
